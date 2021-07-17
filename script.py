0618a3bc0f0e6e90ec262c232a5dd0c8a54590dd
Tuna Tuncer <66808459+kuantuna@users.noreply.github.com>
2021-07-17 04:40:29 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,35 +1,45 @@
-import base64, csv, os, requests
+import base64, copy, csv, os, requests
 
 github_auth_token = os.environ[G_AUTH_TOKEN]
 headers = {
     'Authorization': 'token ' + github_auth_token
 }
-repo_data = []
+repositories = []
 
 # 14*30=420
 # There are 30 repos in every page, so with 14 iterations we get 420 java repositories.
 for i in range(1, 15):
     url = "https://api.github.com/search/repositories?q=language:java&sort=forks&order=desc&page=" + str(i)
     response = requests.get(url=url, headers=headers).json()
-    for item in response["items"]:
-        repo_data.append([item["full_name"], item["html_url"]])
+    for repository in response["items"]:
+        repositories.append({"name": repository["full_name"], "link": repository["html_url"]})
 
-jacoco_repos = [['repo-name', 'repo-link']]
-
-for data in repo_data:
-    response = requests.get(url="https://api.github.com/repos/" + data[0] + "/contents", headers=headers).json()
-    for resp in response:
+filtered_repositories = []
+for repository in repositories:
+    files = requests.get(url="https://api.github.com/repos/" + repository["name"] + "/contents", headers=headers).json()
+    for file in files:
         # checking if the repository has pom.xml in its root
-        if resp["name"] == "pom.xml":
-            pom_response = requests.get(url="https://api.github.com/repos/"+ data[0] +"/contents/pom.xml", headers=headers).json()
-            content_b = base64.b64decode(pom_response["content"])
-            content = content_b.decode("utf-8")
-            # checking if the pom.xml contains org.jacoco keyword 
-            if "org.jacoco" in content:
-                jacoco_repos.append(data)
+        if file["name"] == "pom.xml":
+            repository["build-tool"] = "maven"
+            response = requests.get(url="https://api.github.com/repos/"+ repository["name"] +"/contents/pom.xml", headers=headers).json()
+            pom_content = base64.b64decode(response["content"]).decode("utf-8")
+            # checking if the pom.xml contains "jacoco-maven-plugin" keyword 
+            if "jacoco-maven-plugin" in pom_content:
+                repository["coverage-tool"] = "jacoco"
+                filtered_repositories.append(copy.deepcopy(repository))
+            # checking if the pom.xml contains "cobertura-maven-plugin" keyword 
+            if "cobertura-maven-plugin" in pom_content:
+                repository["coverage-tool"] = "cobertura"
+                filtered_repositories.append(copy.deepcopy(repository))
             break
 
+# Remove duplicates
+final_repositories = []
+[final_repositories.append(repository) for repository in filtered_repositories if repository not in final_repositories]
+
+# Save repositories to a csv file
 with open("data.csv", "w", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
-    for repo in jacoco_repos:
-        csv_writer.writerow([repo[0], repo[1]])
+    csv_writer.writerow(["name", "link", "build-tool", "coverage-tool"])
+    for repository in final_repositories:
+        csv_writer.writerow([repository["name"], repository["link"], repository["build-tool"], repository["coverage-tool"]])
